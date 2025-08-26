9cedfb186c8e7002aab42728db8a98ddac7f0965
dior011 <diyorraxmatullaev3@gmail.com>
2025-08-26 16:03:41 +0500

    template

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useTranslation } from "react-i18next";
+import "./i18n"; // i18n ni import qilish shart!
 
 const App = () => {
+  const { t, i18n } = useTranslation();
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <h3>{t("howItWorks.steps.select.title")}</h3>
+      <h3>{t("hero.title")}</h3>
+      <h3>{t("hero.subtitle")}</h3>
 
-export default App
+      {/* Tilni o‘zgartirish tugmalari */}
+      <button onClick={() => i18n.changeLanguage("uz")}>O‘zbekcha</button>
+      <button onClick={() => i18n.changeLanguage("en")}>English</button>
+      <button onClick={() => i18n.changeLanguage("ru")}>Русский</button>
+    </div>
+  );
+};
+
+export default App;
