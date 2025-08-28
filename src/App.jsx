6fd86e9bb73391401e387b97ed9cd1eb8f2b8797
Abu <abu@gmail.com>
2025-08-28 12:59:01 +0500

    wddssd

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { useTranslation } from "react-i18next";
 import "./i18n"; // i18n ni import qilish shart!
+import Abubakir from "./components/Abubakir";
 
 const App = () => {
   const { t, i18n } = useTranslation();
 
   return (
     <div>
-      <h3>{t("howItWorks.steps.select.title")}</h3>
+    <div className="flex gap-3 bg-gray-200 p-3 ">
+      {/* <h3>{t("howItWorks.steps.select.title")}</h3>
       <h3>{t("hero.title")}</h3>
-      <h3>{t("hero.subtitle")}</h3>
+      <h3>{t("hero.subtitle")}</h3> */}
 
       {/* Tilni o‘zgartirish tugmalari */}
-      <button onClick={() => i18n.changeLanguage("uz")}>O‘zbekcha</button>
-      <button onClick={() => i18n.changeLanguage("en")}>English</button>
-      <button onClick={() => i18n.changeLanguage("ru")}>Русский</button>
+      <button className="bg-black text-white rounded" onClick={() => i18n.changeLanguage("uz")}>O‘zbekcha</button>
+      <button className="bg-black text-white rounded" onClick={() => i18n.changeLanguage("en")}>English</button>
+      <button className="bg-black text-white rounded" onClick={() => i18n.changeLanguage("ru")}>Русский</button>
+      
+    </div>
+    <Abubakir />
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()  ,
-  tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
 })
