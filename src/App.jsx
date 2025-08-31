f5cf804a6d62717ef9f0ed7d10d0c763d1f3eb51
Abu <abu@gmail.com>
2025-08-31 11:02:54 +0500

    vffgf

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,9 @@ const App = () => {
   const { t, i18n } = useTranslation();
 
   return (
-    <div>
-    <div className="flex gap-3 bg-gray-200 p-3 ">
-      {/* <h3>{t("howItWorks.steps.select.title")}</h3>
-      <h3>{t("hero.title")}</h3>
-      <h3>{t("hero.subtitle")}</h3> */}
-
-      {/* Tilni o‘zgartirish tugmalari */}
-      <button className="bg-black text-white rounded" onClick={() => i18n.changeLanguage("uz")}>O‘zbekcha</button>
-      <button className="bg-black text-white rounded" onClick={() => i18n.changeLanguage("en")}>English</button>
-      <button className="bg-black text-white rounded" onClick={() => i18n.changeLanguage("ru")}>Русский</button>
-      
-    </div>
+ <div>
     <Abubakir />
-    </div>
+ </div>
   );
 };
 
